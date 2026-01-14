675931eccfdc6cbe1d7fb2c7260be6cbe368b13c
Franck S. <franck.spadotto@yahoo.com>
2026-01-14 14:47:11 +0100

    feat: add typescript

---
+++ .config/nvim/lua/config/auto-commands/typescript.lua
@@ -0,0 +1,19 @@
+-- Auto-command for TypeScript
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "typescript",
+  callback = function()
+    -- Indentation
+    vim.bo.expandtab = true
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
+
+    -- Activate linting with ESLint if installed
+    vim.cmd([[setlocal formatprg=eslint\ --fix\ --stdin\ --stdin-filename\ %]])
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+	    pattern = "*.ts",
+	    command = "EslintFixAll"
+    })
+    end
+})
