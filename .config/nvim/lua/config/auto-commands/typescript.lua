ba05fc4ae32056a6f2aa77500b037ed846a7fd98
Franck S. <84309973+Franck-dev-hub@users.noreply.github.com>
2026-03-28 18:58:45 +0100

    feat(nvim): update ts linter

--- .config/nvim/lua/config/auto-commands/typescript.lua
+++ .config/nvim/lua/config/auto-commands/typescript.lua
@@ -1,19 +1,21 @@
 -- Auto-command for TypeScript
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "typescript",
+  pattern = {"typescript", "typescriptreact"},
   callback = function()
     -- Indentation
     vim.bo.expandtab = true
     vim.bo.shiftwidth = 2
     vim.bo.tabstop = 2
     vim.bo.softtabstop = 2
-
-    -- Activate linting with ESLint if installed
-    vim.cmd([[setlocal formatprg=eslint\ --fix\ --stdin\ --stdin-filename\ %]])
-
-    vim.api.nvim_create_autocmd("BufWritePre", {
-	    pattern = "*.ts",
-	    command = "EslintFixAll"
+    -- Autoformat with eslint at the save
+    vim.api.nvim_create_autocmd("BufWritePost", {
+      pattern = {"*.ts", "*.tsx"},
+      callback = function()
+        -- Execute npm run fix on the current file
+        vim.cmd("silent !npx eslint --fix " .. vim.fn.expand("%:p"))
+        -- Reload buffer after format
+        vim.cmd("edit!")
+      end
     })
     end
 })
