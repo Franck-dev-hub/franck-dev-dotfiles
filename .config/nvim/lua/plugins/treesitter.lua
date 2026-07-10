ad4483292770294f465f494442ddaa3c4a1fef0d
Franck S. <franck@emagma.fr>
2026-07-10 13:48:47 +0200

    [Update] treesitter version

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,15 @@
-require'nvim-treesitter.configs'.setup {
-	ensure_installed = { "bash", "c", "css", "cpp", "go", "html", "java", "javascript", "json", "lua", "markdown", "markdown_inline", "python", "rust", "tsx", "typescript" },
-	highlight = {
-		enable = true,
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-		additional_vim_regex_highlighting = false,
-	},
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-		init_selection = "gnn", -- set to `false` to disable one of the mappings
-		node_incremental = "grn",
-		scope_incremental = "grc",
-		node_decremental = "grm",
-		},
-	},
-}
+-- New nvim-treesitter API (rewrite, requires Neovim 0.12+)
+-- ensure_installed is now done via install(), highlight via vim.treesitter.start()
+require('nvim-treesitter').install({
+  "bash", "c", "css", "cpp", "go", "html", "java", "javascript",
+  "json", "lua", "markdown", "markdown_inline", "python", "rust",
+  "tsx", "typescript"
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {
+    "bash", "c", "css", "cpp", "go", "html", "java", "javascript",
+    "json", "lua", "markdown", "python", "rust", "tsx", "typescript"
+  },
+  callback = function() vim.treesitter.start() end,
+})
